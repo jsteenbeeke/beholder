230b5034a98eac36aaad8bda11f84dd780b53c3a
Jeroen Steenbeeke <j.steenbeeke@gmail.com>
2016-12-19 16:21:10 +0100

    Update on blur

--- src/main/java/com/jeroensteenbeeke/topiroll/beholder/web/components/mapcontrol/TokenStateController.java
+++ src/main/java/com/jeroensteenbeeke/topiroll/beholder/web/components/mapcontrol/TokenStateController.java
@@ -7,10 +7,9 @@ import javax.inject.Inject;
 
 import org.apache.wicket.AttributeModifier;
 import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.ajax.form.AjaxFormComponentUpdatingBehavior;
 import org.apache.wicket.ajax.markup.html.AjaxLink;
-import org.apache.wicket.ajax.markup.html.form.AjaxSubmitLink;
 import org.apache.wicket.markup.html.basic.Label;
-import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.markup.html.form.NumberTextField;
 import org.apache.wicket.markup.html.list.ListItem;
 import org.apache.wicket.markup.html.list.ListView;
@@ -31,19 +30,26 @@ import com.jeroensteenbeeke.topiroll.beholder.entities.TokenInstance;
 import com.jeroensteenbeeke.topiroll.beholder.entities.filter.TokenInstanceFilter;
 
 public abstract class TokenStateController extends Panel {
+	private static final String ID_MAX_HP = "maxHP";
+
+	private static final String ID_CURRENT_HP = "currentHP";
+
 	private static final long serialVersionUID = 1L;
 
 	@Inject
 	private TokenInstanceDAO tokenDAO;
-	
+
+	@Inject
+	private MapService mapService;
+
 	private IModel<MapView> viewModel;
-	
+
 	private IModel<ScaledMap> mapModel;
 
 	public TokenStateController(String id, MapView view, ScaledMap map) {
 		super(id);
 		setOutputMarkupId(true);
-		
+
 		this.viewModel = ModelMaker.wrap(view);
 		this.mapModel = ModelMaker.wrap(map);
 
@@ -61,11 +67,9 @@ public abstract class TokenStateController extends Panel {
 				.filter(i -> i.isVisible(view, true))
 				.collect(Collectors.toList());
 
-		add(new ListView<TokenInstance>("tokens", ModelMaker.wrap(tokens)) {
+		ListView<TokenInstance> tokenView = new ListView<TokenInstance>(
+				"tokens", ModelMaker.wrap(tokens)) {
 			private static final long serialVersionUID = 1L;
-
-			@Inject
-			private MapService mapService;
 
 			@Override
 			protected void populateItem(ListItem<TokenInstance> item) {
@@ -89,58 +93,70 @@ public abstract class TokenStateController extends Panel {
 							public void onClick(AjaxRequestTarget target) {
 								mapService.setTokenBorderType(
 										item.getModelObject(), type);
-								
+
 								replaceMe(target);
 
 							}
 
 						};
-						
-						link.add(AttributeModifier.replace("class", new LoadableDetachableModel<String>() {
-							private static final long serialVersionUID = 1L;
 
-							@Override
-							protected String load() {
-								if (type == item.getModelObject().getBorderType()) {
-									return "btn btn-primary";
-								}
-								
-								return "btn btn-default";
-							}
-						}));
+						link.add(AttributeModifier.replace("class",
+								new LoadableDetachableModel<String>() {
+									private static final long serialVersionUID = 1L;
+
+									@Override
+									protected String load() {
+										if (type == item.getModelObject()
+												.getBorderType()) {
+											return "btn btn-primary";
+										}
+
+										return "btn btn-default";
+									}
+								}));
 
 						link.setBody(Model.of(type.name()));
 						_item.add(link);
 					}
 				});
+
+				NumberTextField<Integer> currentHitpointsField = new NumberTextField<>(
+						ID_CURRENT_HP, Model.of(instance.getCurrentHitpoints()),
+						Integer.class);
+				NumberTextField<Integer> maxHitpointsField = new NumberTextField<>(
+						ID_MAX_HP, Model.of(instance.getMaxHitpoints()),
+						Integer.class);
 				
-				NumberTextField<Integer> currentHitpointsField = new NumberTextField<>("currentHP", Model.of(instance.getCurrentHitpoints()), Integer.class);
-				NumberTextField<Integer> maxHitpointsField = new NumberTextField<>("maxHP", Model.of(instance.getMaxHitpoints()), Integer.class);
-				
-				Form<TokenInstance> healthForm = new Form<TokenInstance>("health") {
+				currentHitpointsField.add(new AjaxFormComponentUpdatingBehavior("blur") {
 					private static final long serialVersionUID = 1L;
 
 					@Override
-					protected void onSubmit() {
-						mapService.setTokenHP(item.getModelObject(), currentHitpointsField.getModelObject(), maxHitpointsField.getModelObject());
-}					
-					
-				};
-				
-				healthForm.add(currentHitpointsField);
-				healthForm.add(maxHitpointsField);
-				
-				healthForm.add(new AjaxSubmitLink("update", healthForm) {
-					private static final long serialVersionUID = 1L;
-					
-					@Override
-					protected void onSubmit(AjaxRequestTarget target, Form<?> form) {
-						replaceMe(target);
+					protected void onUpdate(AjaxRequestTarget target) {
+						mapService.setTokenHP(item.getModelObject(),
+								currentHitpointsField.getModelObject(),
+								maxHitpointsField.getModelObject());
+
 					}
 				});
-				item.add(healthForm);
 				
-				item.add(new AjaxIconLink<TokenInstance>("hide", item.getModel(), GlyphIcon.eyeClose) {
+				
+				maxHitpointsField.add(new AjaxFormComponentUpdatingBehavior("blur") {
+					private static final long serialVersionUID = 1L;
+
+					@Override
+					protected void onUpdate(AjaxRequestTarget target) {
+						mapService.setTokenHP(item.getModelObject(),
+								currentHitpointsField.getModelObject(),
+								maxHitpointsField.getModelObject());
+						
+					}
+				});
+
+				item.add(currentHitpointsField);
+				item.add(maxHitpointsField);
+
+				item.add(new AjaxIconLink<TokenInstance>("hide",
+						item.getModel(), GlyphIcon.eyeClose) {
 					private static final long serialVersionUID = 1L;
 
 					@Override
@@ -150,25 +166,25 @@ public abstract class TokenStateController extends Panel {
 						replaceMe(target);
 					}
 
-					
 				});
 
 			}
-		});
+		};
 		
+add(tokenView);
 		
 	}
-	
+
 	public ScaledMap getMap() {
 		return mapModel.getObject();
 	}
-	
+
 	@Override
 	protected void onDetach() {
 		super.onDetach();
 		viewModel.detach();
 		mapModel.detach();
 	}
-	
+
 	public abstract void replaceMe(AjaxRequestTarget target);
 }
