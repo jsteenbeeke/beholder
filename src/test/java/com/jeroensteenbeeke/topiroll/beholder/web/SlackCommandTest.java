998a1c41aa97ed341a3d9fe74bc057d315168e05
Jeroen Steenbeeke <j.steenbeeke@gmail.com>
2019-06-14 13:23:45 +0200

    Test improvement

--- src/test/java/com/jeroensteenbeeke/topiroll/beholder/web/SlackCommandTest.java
+++ src/test/java/com/jeroensteenbeeke/topiroll/beholder/web/SlackCommandTest.java
@@ -1,17 +1,17 @@
 /**
  * This file is part of Beholder
  * (C) 2016-2019 Jeroen Steenbeeke
- *
+ * <p>
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- *
+ * <p>
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Affero General Public License for more details.
- *
+ * <p>
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -24,6 +24,8 @@ import com.jeroensteenbeeke.topiroll.beholder.frontend.StartBeholderApplication;
 import io.vavr.collection.HashMap;
 import io.vavr.control.Option;
 import okhttp3.*;
+import org.hamcrest.Description;
+import org.hamcrest.TypeSafeDiagnosingMatcher;
 import org.json.simple.JSONAware;
 import org.json.simple.JSONObject;
 import org.junit.AfterClass;
@@ -35,6 +37,7 @@ import java.net.URLEncoder;
 import java.nio.charset.Charset;
 import java.util.function.Predicate;
 
+import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 
 public abstract class SlackCommandTest {
@@ -82,7 +85,6 @@ public abstract class SlackCommandTest {
 		}
 
 		public void expectingResponse(Predicate<JSONObject> slackResponsePredicate) throws IOException, InterruptedException {
-			System.out.println("Building request");
 			Request request = new Request.Builder().url("http://127.0.0.1:8081/beholder/slack/command")
 												   .header("X-Slack-Signature", "1337")
 												   .header("X-Slack-Request-Timestamp", String.valueOf(System.currentTimeMillis()))
@@ -99,15 +101,49 @@ public abstract class SlackCommandTest {
 
 			Response response = new OkHttpClient().newCall(request).execute();
 
-			assertTrue(response.isSuccessful());
+			assertThat(response, isSuccess());
 
-			Thread.sleep(1000);
+			Thread.sleep(5000);
 
 			Option<JSONObject> lastCommandResponseReceived = FakeSlackServer.getLastCommandResponseReceived();
 			assertTrue(String.format("Last slack response [%s] matches predicate", lastCommandResponseReceived
 				.map(JSONAware::toJSONString)
 				.getOrElse("NOTHING")), lastCommandResponseReceived
 						   .filter(slackResponsePredicate).isDefined());
+		}
+
+		private TypeSafeDiagnosingMatcher<Response> isSuccess() {
+			return new TypeSafeDiagnosingMatcher<>() {
+				@Override
+				protected boolean matchesSafely(Response item, Description mismatchDescription) {
+					boolean match = item.isSuccessful();
+
+					if (!match) {
+						mismatchDescription.appendText("response has status ")
+										   .appendValue(item.code())
+										   .appendText(" (")
+										   .appendText(item.message())
+										   .appendText("), with body: ");
+						try {
+							mismatchDescription
+								.appendText(item.body().string());
+						} catch (IOException e) {
+							mismatchDescription
+								.appendText(" <UNREADABLE, EXCEPTION: ")
+								.appendText(e.getClass().getSimpleName())
+								.appendText(" ")
+								.appendValue(e.getMessage());
+						}
+					}
+
+					return true;
+				}
+
+				@Override
+				public void describeTo(Description description) {
+					description.appendText("response is successful");
+				}
+			};
 		}
 	}
 }
