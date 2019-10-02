07be3a1955d759f944a33175a3260fb85ef218ad
Jeroen Steenbeeke <j.steenbeeke@gmail.com>
2019-10-02 21:20:23 +0200

    Create note entity

---
+++ src/main/java/com/jeroensteenbeeke/topiroll/beholder/dao/DungeonMasterNoteDAO.java
@@ -0,0 +1,7 @@
+package com.jeroensteenbeeke.topiroll.beholder.dao;
+
+import com.jeroensteenbeeke.hyperion.meld.DAO;
+import com.jeroensteenbeeke.topiroll.beholder.entities.DungeonMasterNote;
+import com.jeroensteenbeeke.topiroll.beholder.entities.filter.DungeonMasterNoteFilter;
+
+public interface DungeonMasterNoteDAO extends DAO<DungeonMasterNote, DungeonMasterNoteFilter> {}
--- src/main/java/com/jeroensteenbeeke/topiroll/beholder/entities/ScaledMap.java
+++ src/main/java/com/jeroensteenbeeke/topiroll/beholder/entities/ScaledMap.java
@@ -1,36 +1,35 @@
 /**
- * This file is part of Beholder
- * (C) 2016-2019 Jeroen Steenbeeke
+ * This file is part of Beholder (C) 2016-2019 Jeroen Steenbeeke
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * <p>This program is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU Affero General Public License as published by the Free Software Foundation, either version 3
+ * of the License, or (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * <p>This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU Affero General Public License for more details.
  *
- * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * <p>You should have received a copy of the GNU Affero General Public License along with this
+ * program. If not, see <http://www.gnu.org/licenses/>.
  */
 /**
  * This file is part of Beholder (C) 2016 Jeroen Steenbeeke
  *
- * This program is free software: you can redistribute it and/or modify it under the terms of the
+ * <p>This program is free software: you can redistribute it and/or modify it under the terms of the
  * GNU Affero General Public License as published by the Free Software Foundation, either version 3
  * of the License, or (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
- * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Affero General Public License for more details.
+ * <p>This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
  *
- * You should have received a copy of the GNU Affero General Public License along with this program.
- * If not, see <http://www.gnu.org/licenses/>.
+ * <p>You should have received a copy of the GNU Affero General Public License along with this
+ * program. If not, see <http://www.gnu.org/licenses/>.
  */
 package com.jeroensteenbeeke.topiroll.beholder.entities;
 
+import com.jeroensteenbeeke.hyperion.data.BaseDomainObject;
+import com.jeroensteenbeeke.topiroll.beholder.util.Calculations;
 import java.awt.Dimension;
 import java.io.Serializable;
 import java.sql.Blob;
@@ -40,253 +39,256 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
-
 import java.util.Map;
 import java.util.Set;
 import java.util.function.Function;
 import java.util.stream.Collectors;
-import javax.annotation.Nonnull;
-import javax.persistence.*;
-
-import com.jeroensteenbeeke.hyperion.data.BaseDomainObject;
-import com.jeroensteenbeeke.topiroll.beholder.util.Calculations;
-import org.apache.wicket.model.IModel;
-
 import javax.annotation.CheckForNull;
+import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
+import javax.persistence.*;
+import org.apache.wicket.model.IModel;
 
 @Entity
 public class ScaledMap extends BaseDomainObject implements AmazonStored {
 
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	private static final int MAX_PREVIEW_SIZE = 640;
+    private static final int MAX_PREVIEW_SIZE = 640;
 
-	@Id
-	@SequenceGenerator(sequenceName = "SEQ_ID_ScaledMap", name = "ScaledMap",
-			initialValue = 1, allocationSize = 1)
-	@GeneratedValue(generator = "ScaledMap", strategy = GenerationType.SEQUENCE)
-	@Access(value = AccessType.PROPERTY)
+    @Id
+    @SequenceGenerator(
+            sequenceName = "SEQ_ID_ScaledMap",
+            name = "ScaledMap",
+            initialValue = 1,
+            allocationSize = 1)
+    @GeneratedValue(generator = "ScaledMap", strategy = GenerationType.SEQUENCE)
+    @Access(value = AccessType.PROPERTY)
+    private Long id;
 
-	private Long id;
- 	@ManyToOne(fetch=FetchType.LAZY, optional=true) 	@JoinColumn(name="campaign")
+    @ManyToOne(fetch = FetchType.LAZY, optional = true)
+    @JoinColumn(name = "campaign")
+    private Campaign campaign;
 
-	private Campaign campaign;
+    @Column(nullable = true, name = "amazon_key")
+    private String amazonKey;
 
+    @ManyToOne(fetch = FetchType.LAZY, optional = true)
+    @JoinColumn(name = "folder")
+    private MapFolder folder;
 
+    @OneToMany(mappedBy = "map", fetch = FetchType.LAZY)
+    private List<TokenInstance> tokens = new ArrayList<>();
 
- 	@Column(nullable=true, name="amazon_key")
-	private String amazonKey;
+    @Column(nullable = false)
+    private int basicHeight;
 
+    @Column(nullable = false)
+    private int basicWidth;
 
-	@ManyToOne(fetch = FetchType.LAZY, optional = true)
-	@JoinColumn(name = "folder")
-	private MapFolder folder;
+    @OneToMany(mappedBy = "map", fetch = FetchType.LAZY)
+    private List<FogOfWarGroup> groups = new ArrayList<>();
 
+    @OneToMany(mappedBy = "map", fetch = FetchType.LAZY)
+    private List<FogOfWarShape> fogOfWarShapes = new ArrayList<>();
 
-	@OneToMany(mappedBy = "map", fetch = FetchType.LAZY)
-	private List<TokenInstance> tokens = new ArrayList<>();
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "owner")
+    private BeholderUser owner;
 
+    @Column(nullable = false)
+    private String name;
 
-	@Column(nullable = false)
-	private int basicHeight;
+    @Column(nullable = false)
+    private int squareSize;
 
-	@Column(nullable = false)
-	private int basicWidth;
+    @OneToMany(mappedBy = "selectedMap", fetch = FetchType.LAZY)
+    private List<MapView> selectedBy = new ArrayList<>();
 
-	@OneToMany(mappedBy = "map", fetch = FetchType.LAZY)
-	private List<FogOfWarGroup> groups = new ArrayList<>();
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "map")
+    private List<DungeonMasterNote> notes = new ArrayList<>();
 
-	@OneToMany(mappedBy = "map", fetch = FetchType.LAZY)
-	private List<FogOfWarShape> fogOfWarShapes = new ArrayList<>();
+    public Long getId() {
+        return id;
+    }
 
-	@ManyToOne(fetch = FetchType.LAZY, optional = false)
-	@JoinColumn(name = "owner")
-	private BeholderUser owner;
+    public void setId(@Nonnull Long id) {
+        this.id = id;
+    }
 
-	@Column(nullable = false)
-	private String name;
+    @Override
+    public final Serializable getDomainObjectId() {
+        return getId();
+    }
 
-	@Column(nullable = false)
-	private int squareSize;
+    @Nonnull
+    public List<MapView> getSelectedBy() {
+        return selectedBy;
+    }
 
+    public void setSelectedBy(@Nonnull List<MapView> selectedBy) {
+        this.selectedBy = selectedBy;
+    }
 
-	@OneToMany(mappedBy = "selectedMap", fetch = FetchType.LAZY)
-	private List<MapView> selectedBy = new ArrayList<>();
+    public int getSquareSize() {
+        return squareSize;
+    }
 
-	public Long getId() {
-		return id;
-	}
+    public void setSquareSize(int squareSize) {
+        this.squareSize = squareSize;
+    }
 
-	public void setId(@Nonnull Long id) {
-		this.id = id;
-	}
+    @Nonnull
+    public String getName() {
+        return name;
+    }
 
-	@Override
-	public final Serializable getDomainObjectId() {
-		return getId();
-	}
+    public void setName(@Nonnull String name) {
+        this.name = name;
+    }
 
-	@Nonnull
-	public List<MapView> getSelectedBy() {
-		return selectedBy;
-	}
+    @Nonnull
+    public BeholderUser getOwner() {
+        return owner;
+    }
 
-	public void setSelectedBy(@Nonnull List<MapView> selectedBy) {
-		this.selectedBy = selectedBy;
-	}
+    public void setOwner(@Nonnull BeholderUser owner) {
+        this.owner = owner;
+    }
 
-	public int getSquareSize() {
-		return squareSize;
-	}
+    @Nonnull
+    public List<FogOfWarShape> getFogOfWarShapes() {
+        return fogOfWarShapes;
+    }
 
-	public void setSquareSize(int squareSize) {
-		this.squareSize = squareSize;
-	}
+    public void setFogOfWarShapes(@Nonnull List<FogOfWarShape> fogOfWarShapes) {
+        this.fogOfWarShapes = fogOfWarShapes;
+    }
 
-	@Nonnull
-	public String getName() {
-		return name;
-	}
+    @Nonnull
+    public List<FogOfWarGroup> getGroups() {
+        return groups;
+    }
 
-	public void setName(@Nonnull String name) {
-		this.name = name;
-	}
+    public void setGroups(@Nonnull List<FogOfWarGroup> groups) {
+        this.groups = groups;
+    }
 
-	@Nonnull
-	public BeholderUser getOwner() {
-		return owner;
-	}
+    @Transient
+    public double getPreviewFactor() {
+        int w = (int) getBasicWidth();
+        double factor = 1.0;
 
-	public void setOwner(@Nonnull BeholderUser owner) {
-		this.owner = owner;
-	}
+        while (w > MAX_PREVIEW_SIZE) {
+            w = (int) (w * 0.9);
+            factor = 0.9 * factor;
+        }
 
-	@Nonnull
-	public List<FogOfWarShape> getFogOfWarShapes() {
-		return fogOfWarShapes;
-	}
+        return factor;
+    }
 
-	public void setFogOfWarShapes(@Nonnull List<FogOfWarShape> fogOfWarShapes) {
-		this.fogOfWarShapes = fogOfWarShapes;
-	}
+    @Transient
+    public Dimension getPreviewDimension() {
 
-	@Nonnull
-	public List<FogOfWarGroup> getGroups() {
-		return groups;
-	}
+        return new Dimension(
+                (int) (getPreviewFactor() * getBasicWidth()),
+                (int) (getPreviewFactor() * getBasicHeight()));
+    }
 
-	public void setGroups(@Nonnull List<FogOfWarGroup> groups) {
-		this.groups = groups;
-	}
+    @Transient
+    public Dimension getDisplayDimension(MapView mapView) {
+        double factor = getDisplayFactor(mapView);
 
-	@Transient
-	public double getPreviewFactor() {
-		int w = (int) getBasicWidth();
-		double factor = 1.0;
+        return new Dimension((int) (getBasicWidth() * factor), (int) (getBasicHeight() * factor));
+    }
 
-		while (w > MAX_PREVIEW_SIZE) {
-			w = (int) (w * 0.9);
-			factor = 0.9 * factor;
-		}
+    @Transient
+    public double getDisplayFactor(MapView mapView) {
+        return Calculations.scale(getSquareSize())
+                .toResolution(mapView.toResolution())
+                .onScreenWithDiagonalSize(mapView.getScreenDiagonalInInches());
+    }
 
-		return factor;
-	}
+    @Nonnull
+    public int getBasicWidth() {
+        return basicWidth;
+    }
 
-	@Transient
-	public Dimension getPreviewDimension() {
+    public void setBasicWidth(@Nonnull int basicWidth) {
+        this.basicWidth = basicWidth;
+    }
 
-		return new Dimension((int) (getPreviewFactor() * getBasicWidth()),
-				(int) (getPreviewFactor() * getBasicHeight()));
-	}
+    @Nonnull
+    public int getBasicHeight() {
+        return basicHeight;
+    }
 
-	@Transient
-	public Dimension getDisplayDimension(MapView mapView) {
-		double factor = getDisplayFactor(mapView);
+    public void setBasicHeight(@Nonnull int basicHeight) {
+        this.basicHeight = basicHeight;
+    }
 
-		return new Dimension((int) (getBasicWidth() * factor),
-				(int) (getBasicHeight() * factor));
-	}
+    @Nonnull
+    public List<TokenInstance> getTokens() {
+        return tokens;
+    }
 
-	@Transient
-	public double getDisplayFactor(MapView mapView) {
-		return Calculations.scale(getSquareSize())
-				.toResolution(mapView.toResolution())
-				.onScreenWithDiagonalSize(mapView.getScreenDiagonalInInches());
-	}
+    public void setTokens(@Nonnull List<TokenInstance> tokens) {
+        this.tokens = tokens;
+    }
 
+    @CheckForNull
+    public MapFolder getFolder() {
+        return folder;
+    }
 
-	@Nonnull
-	public int getBasicWidth() {
-		return basicWidth;
-	}
+    public void setFolder(@Nullable MapFolder folder) {
+        this.folder = folder;
+    }
 
-	public void setBasicWidth(@Nonnull int basicWidth) {
-		this.basicWidth = basicWidth;
-	}
+    public List<FogOfWarShape> getAllShapes() {
+        Map<Long, FogOfWarShape> shapes =
+                getFogOfWarShapes().stream()
+                        .collect(Collectors.toMap(FogOfWarShape::getId, Function.identity()));
+        getGroups().stream()
+                .flatMap(g -> g.getShapes().stream())
+                .filter(s -> !shapes.containsKey(s.getId()))
+                .forEach(s -> shapes.put(s.getId(), s));
+        return new ArrayList<>(shapes.values());
+    }
 
-	@Nonnull
-	public int getBasicHeight() {
-		return basicHeight;
-	}
+    @CheckForNull
+    @Override
+    public String getAmazonKey() {
+        return amazonKey;
+    }
 
-	public void setBasicHeight(@Nonnull int basicHeight) {
-		this.basicHeight = basicHeight;
-	}
+    public void setAmazonKey(@Nullable String amazonKey) {
+        this.amazonKey = amazonKey;
+    }
 
-	@Nonnull
-	public List<TokenInstance> getTokens() {
-		return tokens;
-	}
+    public String getNameWithFolders() {
+        if (folder != null) {
+            return String.format("%s \\ %s", folder.getNameWithParents(), getName());
+        }
 
-	public void setTokens(@Nonnull List<TokenInstance> tokens) {
-		this.tokens = tokens;
-	}
+        return getName();
+    }
 
-	@CheckForNull
-	public MapFolder getFolder() {
-		return folder;
-	}
+    @CheckForNull
+    public Campaign getCampaign() {
+        return campaign;
+    }
 
-	public void setFolder(@Nullable MapFolder folder) {
-		this.folder = folder;
-	}
+    public void setCampaign(@Nullable Campaign campaign) {
+        this.campaign = campaign;
+    }
 
+    @Nonnull
+    public List<DungeonMasterNote> getNotes() {
+        return notes;
+    }
 
-	public List<FogOfWarShape> getAllShapes() {
-		Map<Long, FogOfWarShape> shapes = getFogOfWarShapes().stream()
-				.collect(Collectors.toMap(FogOfWarShape::getId,
-						Function.identity()));
-		getGroups().stream().flatMap(g -> g.getShapes().stream())
-				.filter(s -> !shapes.containsKey(s.getId())).forEach(s -> shapes.put(s.getId(), s));
-		return new ArrayList<>(shapes.values());
-	}
-
-	@CheckForNull
-	@Override
-	public String getAmazonKey() {
-		return amazonKey;
-	}
-	public void setAmazonKey( @Nullable String amazonKey) {
-		this.amazonKey = amazonKey;
-	}
-
-	
-	public String getNameWithFolders() {
-		if (folder != null) {
-			return String.format("%s \\ %s", folder.getNameWithParents(), getName());
-		}
-
-		return getName();
-	}
-
-	@CheckForNull
-	public Campaign getCampaign() {
-		return campaign;
-	}
-	public void setCampaign( @Nullable Campaign campaign) {
-		this.campaign = campaign;
-	}
-
-
+    public void setNotes(@Nonnull List<DungeonMasterNote> notes) {
+        this.notes = notes;
+    }
 }
