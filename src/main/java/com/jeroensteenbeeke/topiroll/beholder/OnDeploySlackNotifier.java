1741e885adaefeca97769b4a5e928240244b40eb
Jeroen Steenbeeke <j.steenbeeke@gmail.com>
2021-10-11 12:16:16 +0200

    Rewrote deploy notifications

--- src/main/java/com/jeroensteenbeeke/topiroll/beholder/OnDeploySlackNotifier.java
+++ src/main/java/com/jeroensteenbeeke/topiroll/beholder/OnDeploySlackNotifier.java
@@ -1,11 +1,20 @@
 package com.jeroensteenbeeke.topiroll.beholder;
 
 import com.jeroensteenbeeke.hyperion.Hyperion;
+import com.jeroensteenbeeke.hyperion.social.api.webhook.WebhookFailure;
+import com.jeroensteenbeeke.hyperion.social.api.webhook.WebhookInvocation;
+import com.jeroensteenbeeke.hyperion.social.api.webhook.WebhookPayload;
+import com.jeroensteenbeeke.hyperion.social.api.webhook.WebhookResponse;
+import com.jeroensteenbeeke.hyperion.social.api.webhook.blockkit.Divider;
+import com.jeroensteenbeeke.hyperion.social.api.webhook.blockkit.MarkdownText;
+import com.jeroensteenbeeke.hyperion.social.api.webhook.blockkit.PlainText;
+import com.jeroensteenbeeke.hyperion.social.api.webhook.blockkit.Section;
 import com.jeroensteenbeeke.hyperion.solstice.spring.ApplicationMetadataStore;
 import com.jeroensteenbeeke.lux.ActionResult;
 import com.jeroensteenbeeke.topiroll.beholder.beans.DeployNotificationContext;
 import io.vavr.collection.Array;
 import io.vavr.control.Option;
+import io.vavr.control.Try;
 import okhttp3.*;
 import org.apache.wicket.Application;
 import org.apache.wicket.IApplicationListener;
@@ -19,8 +28,8 @@ public class OnDeploySlackNotifier implements IApplicationListener {
 	private static final Logger log = LoggerFactory.getLogger(OnDeploySlackNotifier.class);
 	private static final String DIVIDER = "{\n\t\t\"type\": \"divider\"\n\t}";
 	private static final String START_FIELDS = "{\n" +
-		"\t\t\"type\": \"section\",\n" +
-		"\t\t\"fields\": [";
+			"\t\t\"type\": \"section\",\n" +
+			"\t\t\"fields\": [";
 	private static final String END_FIELDS = "]}";
 
 	private final DeployNotificationContext deployNotificationContext;
@@ -28,8 +37,6 @@ public class OnDeploySlackNotifier implements IApplicationListener {
 	private final ApplicationMetadataStore metadataStore;
 
 	private final ServletContext servletContext;
-
-	private static final OkHttpClient client = new OkHttpClient();
 
 
 	public OnDeploySlackNotifier(DeployNotificationContext deployNotificationContext, ApplicationMetadataStore metadataStore, ServletContext servletContext) {
@@ -42,119 +49,88 @@ public class OnDeploySlackNotifier implements IApplicationListener {
 	public void onAfterInitialized(Application application) {
 		final String runningApplicationHash = String.format("%s:%s:%s",
 															BeholderApplication.get().getRevision(),
-															Hyperion.getRevision().getOrElse("Unknown"),
+															Hyperion
+																	.getRevision()
+																	.getOrElse("Unknown"),
 															Option
-																.of(System.getenv("DOCKER_IMAGE_ID"))
-																.getOrElse("Unknown")
+																	.of(System.getenv("DOCKER_IMAGE_ID"))
+																	.getOrElse("Unknown")
 		);
 
 		Option<String> previousApplicationHash = metadataStore.readString(BeholderApplication.KEY_BEHOLDER_CURRENT_VERSION);
 
 		if (previousApplicationHash.isEmpty() || previousApplicationHash
-			.filter(p -> !p.equals(runningApplicationHash)).isDefined()) {
+				.filter(p -> !p.equals(runningApplicationHash)).isDefined()) {
 			ActionResult writeResult = metadataStore
-				.writeString(BeholderApplication.KEY_BEHOLDER_CURRENT_VERSION, runningApplicationHash);
+					.writeString(BeholderApplication.KEY_BEHOLDER_CURRENT_VERSION, runningApplicationHash);
 			writeResult
-				.ifOk(() -> {
+					.ifOk(() -> {
 
-					String deployWebhook = deployNotificationContext.getDeployWebhook();
-					String message = "A new version of Beholder was just deployed to *%s* by *%s*".formatted(deployNotificationContext.getEnvironmentName(), deployNotificationContext.getDeployingInstance());
-					String attachments = createAttachments();
+						String deployWebhook = deployNotificationContext.getDeployWebhook();
+						String message = "A new version of Beholder was just deployed to *%s* by *%s*".formatted(deployNotificationContext.getEnvironmentName(), deployNotificationContext.getDeployingInstance());
 
-					String payload = String.format("{\n\t\"text\": \"%s\",\n\t\"attachments\": [{\"blocks\": [%s]}]\n}", message, attachments);
-
-					if (deployWebhook != null && !deployWebhook.isEmpty()) {
-						log.info("Posting payload to Slack: {}", payload);
-
-						Request request = new Request.Builder()
-							.post(RequestBody.create(MediaType.parse("application/json"),
-													 payload))
-							.url(deployWebhook) //post in designated channel
-							.build();
+						Try<WebhookResponse> response = WebhookInvocation
+								.invoke(deployWebhook)
+								.withPayload(
+										createPayload(message)
 
 
-						try (Response response = client.newCall(request).execute()) {
-							if (!response.isSuccessful()) {
-								ResponseBody body = response
-									.body();
-								log.error("Failed to post deploy message to Slack webhook: {} {}\n{}", response.code(), response
-									.message(), body
-											  .string());
+								);
+
+						response.onSuccess(r -> {
+							if (r instanceof WebhookFailure failure) {
+								log.error("Could not send deploy notification. Webhook returned {}: {}\n{}", failure.code(), failure.message(), failure.body());
+							} else {
+								log.info("Deploy notification sent");
 							}
-						} catch (IOException e) {
-							log.error(e.getMessage(), e);
-						}
-					}
-				});
+
+						}).onFailure(t -> log.error("Could not send deploy notification", t));
+					});
 		}
 	}
 
-	private String createAttachments() {
-		StringBuilder sb = new StringBuilder();
+	private WebhookPayload createPayload(String message) {
+		return new WebhookPayload(message)
+				.withBlock(Section
+								   .sectionWithoutHeader(new MarkdownText("*Environment*"))
+								   .withField(new PlainText(deployNotificationContext.getEnvironmentName()))
+								   .withField(new MarkdownText("*Commit Message*"))
+								   .withField(new PlainText(BeholderApplication
+																	.get()
+																	.getCommitMessage()))
+								   .withField(new MarkdownText("*Beholder Revision*"))
+								   .withField(new PlainText(BeholderApplication
+																	.get()
+																	.getRevision()))
+								   .withField(new MarkdownText("*Hyperion Revision*"))
+								   .withField(new PlainText(Hyperion
+																	.getRevision()
+																	.getOrElse("unknown")))
+								   .withField(new MarkdownText("*Hyperion Commit Message*"))
+								   .withField(new PlainText(Hyperion
+																	.getCommitTitle()
+																	.getOrElse("unknown")))
 
-		Array<String> fields = Array.empty();
+				).withBlock(Section.sectionWithoutHeader(new MarkdownText("*Docker image ID*"))
+								   .withField(new MarkdownText(Option
+																	   .of(System.getenv("DOCKER_IMAGE_ID"))
+																	   .map(this::asCode)
+																	   .getOrElse("unknown")))
+								   .withField(new MarkdownText("*JDK*"))
+								   .withField(new PlainText(System.getProperty("java.vendor") + " " + Runtime
+										   .version()
+										   .toString()))
+								   .withField(new MarkdownText("*Server*"))
+								   .withField(new PlainText(servletContext.getServerInfo()))
+				).withBlock(new Divider())
+				.withBlock(Section.sectionWithHeader(new MarkdownText("**Commit details*\\n" + BeholderApplication
+						.get()
+						.getCommitDetails()
+						.replace("\n", "\\n"))));
 
-		fields = fields.append(field("mrkdwn", "*Environment*"));
-		fields = fields.append(field("plain_text", BeholderApplication.get().getCommitMessage()));
-		fields = fields.append(field("mrkdwn", "*Commit Message*"));
-		fields = fields.append(field("plain_text", BeholderApplication.get().getCommitMessage()));
-		fields = fields.append(field("mrkdwn", "*Beholder Revision*"));
-		fields = fields.append(field("plain_text", BeholderApplication.get().getRevision()));
-		fields = fields.append(field("mrkdwn", "*Hyperion Revision*"));
-		fields = fields.append(field("plain_text", Hyperion.getRevision().getOrElse("unknown")));
-		Option<String> hyperionCommitTitle = Hyperion.getCommitTitle();
-		if (hyperionCommitTitle.isDefined()) {
-			fields = fields.append(field("mrkdwn", "*Hyperion Commit Message*"));
-			fields = fields.append(field("plain_text", hyperionCommitTitle.getOrElse("unknown")));
-		}
-
-		String imageInfo = "";
-
-		String dockerImageID = System.getenv("DOCKER_IMAGE_ID");
-		if (dockerImageID != null && !dockerImageID.isEmpty()) {
-			imageInfo += String.format("*ID:* `%s`\\n", dockerImageID);
-		}
-
-		imageInfo += String.format("*JDK:* `%s`\\n", System.getProperty("java.vendor") + " " + Runtime
-			.version()
-			.toString());
-
-		imageInfo += String.format("*Server:* `%s`\\n", servletContext.getServerInfo());
-
-		fields = fields.append(field("mrkdwn", "*Image Info*"));
-		fields = fields.append(field("mrkdwn", imageInfo));
-
-		sb.append(fields.mkString(START_FIELDS, ",\n\t\t", END_FIELDS));
-
-		String details = BeholderApplication.get().getCommitDetails().replace("\n", "\\n");
-
-		if (!details.isEmpty() && !details.equals("Unknown")) {
-			sb.append(",\n\t");
-			sb.append(DIVIDER);
-			sb.append(",\n\t");
-
-			sb
-				.append("""
-								{
-								\t\t"type": "section",
-								\t\t"text": {
-								\t\t\t"type": "mrkdwn",
-								\t\t\t"text": "*Commit details*\\n""")
-				.append(details)
-				.append("\"\n")
-				.append("\t\t}\n")
-				.append("\t}");
-		}
-
-
-		return sb.toString();
 	}
 
-	public String field(String type, String text) {
-		return "{\n" +
-			"\t\t\t\t\"type\": \"" + type + "\",\n" +
-			"\t\t\t\t\"text\": \"" + text + "\"\n" +
-			"\t\t\t}";
+	private String asCode(String s) {
+		return "`" + s + "`";
 	}
-
 }
